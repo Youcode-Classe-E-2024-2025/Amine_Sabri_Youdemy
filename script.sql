79dfe0bf8f89c93b9912e9248a760af93f46bac5
sabri-amine <sabriamine368@gmail.com>
2025-01-14 10:36:45 +0000

    ajouté les table roles et users dans fichier script.sql

--- script.sql
+++ script.sql
@@ -3,3 +3,15 @@ CREATE TABLE roles (
     name ENUM('admin', 'enseignant', 'etudiant') NOT NULL
 );
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL DEFAULT 3, 
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE RESTRICT
+);
+
+
+

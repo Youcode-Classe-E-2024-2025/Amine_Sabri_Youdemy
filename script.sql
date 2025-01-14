096fe364f3e84319b3123c611b37285e1f227cae
sabri-amine <sabriamine368@gmail.com>
2025-01-14 10:37:34 +0000

    ajouté les table categories et courses dans fichier script.sql

--- script.sql
+++ script.sql
@@ -15,3 +15,22 @@ CREATE TABLE users (
 
 
 
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+
+
+CREATE TABLE courses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    video_url VARCHAR(255),
+    image_url VARCHAR(255),
+    document_url VARCHAR(255),
+    category_id INT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
+);
+

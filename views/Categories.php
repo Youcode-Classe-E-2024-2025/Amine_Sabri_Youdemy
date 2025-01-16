196d2a068d67581caffe3bfd8023ab9a433c13d5
sabri-amine <sabriamine368@gmail.com>
2025-01-16 14:25:14 +0000

    ajouter form de ajouter  des categories et form pour modifier categories

--- views/Categories.php
+++ views/Categories.php
@@ -75,3 +75,27 @@
     </div>
 
 
+    <?php if (isset($_GET['id'])): ?>
+    <div class="mt-8">
+        <h2 class="text-2xl font-semibold text-gray-700 mb-4">Modifier la Catégorie</h2>
+        <?php
+        $categoryId = $_GET['id'];
+        // var_dump($categoryId);
+        $cat = new CategoryController();
+        $category = $cat->getCategoryById($categoryId);
+        if ($category): ?>
+            <form action="../../index.php?action=editCategories" method="POST" class="space-y-4">
+                <input type="hidden" name="id" value="<?= $category['id']; ?>">
+                <div>
+                    <label for="category_name" class="block text-gray-700">Nom de la catégorie</label>
+                    <input type="text" name="name" id="name" value="<?= htmlspecialchars($category['name']); ?>" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                </div>
+                <div>
+                    <button type="submit" class="w-full bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600">Modifier</button>
+                </div>
+            </form>
+        <?php else: ?>
+            <p class="text-red-500">Catégorie non trouvée.</p>
+        <?php endif; ?>
+    </div>
+<?php endif; ?>

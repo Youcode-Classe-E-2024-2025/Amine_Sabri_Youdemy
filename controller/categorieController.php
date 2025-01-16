9f263a619055f566814b1d8b4ecef9c6fdb2e013
sabri-amine <sabriamine368@gmail.com>
2025-01-16 14:30:51 +0000

    ajouter categorieController avec createion des categories

---
+++ controller/categorieController.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__ . '/../model/categorie.php';
+class CategoryController {
+    private $model;
+
+    public function __construct() {
+        $this->model = new Category();
+    }
+
+    public function createCategory() {
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $name = $_POST["category_name"];
+            $this->model->setName($name);
+            $Categories = $this->model->create();
+            header('Location: views/layouts/dashbord.php?page=categories');
+        }
+    }
+
+}
+?>

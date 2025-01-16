9f1a77726ea79ecfb9ef43dcf45e93ebe5df4a6b
sabri-amine <sabriamine368@gmail.com>
2025-01-16 14:24:32 +0000

    ajouter supprision  des categories

--- views/Categories.php
+++ views/Categories.php
@@ -61,3 +61,17 @@
 
 
 
+<div class="mt-8">
+        <h2 class="text-2xl font-semibold text-gray-700 mb-4">Ajouter une Catégorie</h2>
+        <form action="../../index.php?action=createCategories" method="POST" class="space-y-4">
+            <div>
+                <label for="category_name" class="block text-gray-700">Nom de la catégorie</label>
+                <input type="text" name="category_name" id="category_name" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+            </div>
+            <div>
+                <button type="submit" class="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Ajouter</button>
+            </div>
+        </form>
+    </div>
+
+

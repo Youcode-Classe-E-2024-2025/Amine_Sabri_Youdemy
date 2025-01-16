d583909d14c5669cccccc4f284fcef6fd3faea77
sabri-amine <sabriamine368@gmail.com>
2025-01-16 14:22:14 +0000

    ajouter sidebar de dashbord  dans file include

---
+++ views/include/sidebar.php
@@ -0,0 +1,17 @@
+<aside class="w-64 bg-gray-800 text-white p-6">
+    <div class="text-2xl font-bold text-primary flex items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 mr-2" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838l-2.727 1.17 1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zm5.99 7.176A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"/>
+        </svg>
+        Youdemy
+    </div>
+    <nav>
+        <ul class="space-y-2 mt-6">
+            <li><a href="dashbord.php?page=accueil" class="block py-2 px-4 rounded hover:bg-gray-700">Accueil</a></li>
+            <li><a href="dashbord.php?page=statistiques" class="block py-2 px-4 rounded hover:bg-gray-700">Statistiques</a></li>
+            <li><a href="dashbord.php?page=categories" class="block py-2 px-4 rounded hover:bg-gray-700">Categorey</a></li>
+            <li><a href="dashbord.php?page=Tags" class="block py-2 px-4 rounded hover:bg-gray-700">Tags</a></li>
+            <li><a href="dashbord.php?page=Cours" class="block py-2 px-4 rounded hover:bg-gray-700">Cours</a></li>
+        </ul>
+    </nav>
+</aside>
